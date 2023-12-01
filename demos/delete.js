e1a4ea7f602ab52a7dc94b4885745f55b22bc673
Admin <>
2023-12-01 02:08:25 -0800

    deleteJS

--- demos/delete.js
+++ demos/delete.js
@@ -1,9 +1,21 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import {} from 'dotenv/config'
 
 //DELETE DOCUMENT
 MongoClient.connect(process.env.DB, (err, database) => {
     if (err) throw err
     console.log('Connected to database...')
-    
+
+   // let query = { name: 'Delete User' }
+    let query = { _id: ObjectId('6569aa0870b0e35923230ad7') }
+
+    database
+            .db('vectacorp')
+            .collection('employees')
+            deleteOne(query, (err, res) => {
+                if (err) throw err
+                console.log('1 document deleted')
+                database.close()
+            })
+
 })
