e681d1c014550feaebf04683fcdcdeadefbef69e
Admin <>
2023-12-01 01:19:08 -0800

    retrieveDocs

---
+++ demos/retrieve-one.js
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb';
+import {} from 'dotenv/config';
+
+//Retrieve ONE
+// MongoClient.connect(process.env.DB, (err, database) => {
+//     if (err) throw err
+//     console.log('Connected to database...')
+
+//     database
+//         .db('vectacorp')
+//         .collection('employees')
+//         .findOne({}, (err, res) => {
+//              if (err) throw err
+//             console.log(res)
+//             database.close()
+//         })
+// })
+
+//QUERY
+MongoClient.connect(process.env.DB, (err, database) => {
+    if (err) throw err
+    console.log('Connected to database...')
+
+    let query = { name : 'Carlos Bravo'}
+    database
+        .db('vectacorp')
+        .collection('employees')
+        .find(query)
+        .toArray((err, res) => {
+             if (err) throw err
+            console.log(res)
+            database.close()
+        })
+})
