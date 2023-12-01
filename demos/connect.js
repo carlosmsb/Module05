f873c8c33228c905224606bbb24850d87191b2e2
Admin <>
2023-12-01 00:38:26 -0800

    DBConnect

--- demos/connect.js
+++ demos/connect.js
@@ -1,24 +1,30 @@
 import { MongoClient } from 'mongodb'
 import {} from 'dotenv/config'
-
 const uri = process.env.DB
 const client = new MongoClient(uri)
-
-async function retrieveAll() {
+//  CONNECT TO DATABASE
+async function insertOne() {
     try {
         await client.connect(uri)
         console.log('Connected to database...')
-
-        let database = client.db('sample_airbnb')
-        let result = await database
-            .collection('listingsAndReviews')
-            .find({})
-            .toArray()
-        console.log(result)
+        let database = client.db('vectacorp')
+        let employee = {
+            name: 'Employee Name',
+            extension: 1111,
+            email: 'employee@vectacorp.com',
+            title: 'Software Engineer',
+            dateHired: Date.now(),
+            currentlyEmployed: true
+        }
+        const result = await database
+            .collection('employees')
+            .insertOne(employee)
+            console.log(result)
+            console.log('1 document inserted into collection.')
     } catch (err) {
         console.log(err)
     } finally {
         await client.close()
     }
 }
-retrieveAll()
+insertOne()
